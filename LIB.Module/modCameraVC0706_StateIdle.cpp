20dfa104f577dd4e3611f2a7b25b25e9228a2fbb
Sergei <maslennikovserge@yandex.ru>
2021-12-20 21:07:59 +0300

    temp-10

--- LIB.Module/modCameraVC0706_StateIdle.cpp
+++ LIB.Module/modCameraVC0706_StateIdle.cpp
@@ -42,6 +42,22 @@ bool tCameraVC0706::tStateIdle::Go()
 			return false;
 
 		//MsgStatus
+		if (FBufLen.Value > 0)
+		{
+			const std::uint32_t ChunkAddr = 0;
+			const std::uint32_t ChunkSize = 4096;//[TBD] from config ImageChunkSizeMax
+			const std::uint32_t ChunkDelay = 5000;//in 0.01ms
+
+			utils::tVectorUInt8 Data[4096];
+
+			if (!HandleCmd(tPacketCmd::MakeReadFBufCurrent(m_pObj->m_SN, ChunkAddr, ChunkSize, ChunkDelay), MsgStatus, 100) || MsgStatus != tMsgStatus::None)
+				return false;
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));//[TEST]
+		}
+
+		if (!HandleCmd(tPacketCmd::MakeFBufCtrlResumeFrame(m_pObj->m_SN), MsgStatus, 100) || MsgStatus != tMsgStatus::None)
+			return false;
 
 		m_pObj->m_pLog->WriteLine(true, utils::tLogColour::Green, "MEDVED");//[TBD]makes no sense
 	}
