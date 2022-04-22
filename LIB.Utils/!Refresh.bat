afa7acf3fb7b6a4e2ab4cd96cc78494e094d293e
Sergei <maslennikovserge@yandex.ru>
2022-04-22 12:08:50 +0300

    Add: main looks for config path

--- LIB.Utils/!Refresh.bat
+++ LIB.Utils/!Refresh.bat
@@ -6,6 +6,7 @@ xcopy /Y %LIB_UTILS%\utilsChrono.*
 xcopy /Y %LIB_UTILS%\utilsLog.*
 xcopy /Y %LIB_UTILS%\utilsPacket.*
 xcopy /Y %LIB_UTILS%\utilsPacketCameraVC0706.*
+xcopy /Y %LIB_UTILS%\utilsPath.*
 xcopy /Y %LIB_UTILS%\utilsSerialPort.*
 xcopy /Y %LIB_UTILS%\utilsShell.*
 
