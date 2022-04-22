a00cf1e5e9316d82220a04dd76f6085059111c4c
Sergei <maslennikovserge@yandex.ru>
2022-04-22 12:08:50 +0300

    Add: main looks for config path

--- CameraVC0706/main.cpp
+++ CameraVC0706/main.cpp
@@ -6,6 +6,7 @@
 #include <devShell.h>
 
 #include <utilsBase.h>
+#include <utilsPath.h>
 
 #include <atomic>
 #include <chrono>
@@ -130,7 +131,10 @@ int main(int argc, char* argv[])
 		if (PathFile.has_extension())
 			PathFile.replace_extension();
 
-		const std::string FileNameConf = PathFile.string() + ".conf";
+		std::string FileNameConf = utils::GetPathConfig(PathFile.string());
+		if (FileNameConf.empty())
+			throw std::runtime_error{ "config file is not found" };
+
 		dev::g_Settings = dev::tSettings(FileNameConf);
 	}
 	catch (std::exception& e)
